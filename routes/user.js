91afda18ab7a4ed37fd67e8f3a67a78cf1ab6089
Sarvesh SP <sarvesh.sp2013@gmail.com>
2021-07-25 21:42:12 +0530

    added user controller

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Connected mofo");
-});
+const { yo } = require("../controllers/user");
+
+router.get("/", yo);
 
 module.exports = router;
