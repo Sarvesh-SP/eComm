04ff056c26f411b918d8e4ac869af2f8ad8aace4
Sarvesh SP <sarvesh.sp2013@gmail.com>
2021-07-25 22:21:15 +0530

    Added morgan and cookie-parser

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const userRouter = require("./routes/user");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+//env
 require("dotenv").config();
 
 const port = process.env.PORT || 3000;
+
+//import Routes
+const userRouter = require("./routes/user");
 
 //app
 const app = express();
@@ -17,7 +22,13 @@ mongoose
   })
   .then(() => console.log("DB Connected"));
 
-//Routes
+//middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//Routes middleware
 app.use("/", userRouter);
 
 app.listen(port, () => {
