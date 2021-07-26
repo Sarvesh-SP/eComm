4ed8d3b1fd3e28121dbd6caf0aa69951a6d7e784
Sarvesh SP <sarvesh.sp2013@gmail.com>
2021-07-26 23:01:29 +0530

    express-validator fix

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+
 //env
 require("dotenv").config();
 
