a9133b5973d8a2f86b0cc9f6d852de0ab7fa6e34
Sarvesh SP <sarvesh.sp2013@gmail.com>
2021-08-26 18:37:54 +0530

    added user controller

---
+++ controllers/user.js
@@ -0,0 +1,12 @@
+const User = require("../models/user");
+
+exports.userById = (req, res, next, id) => {
+  User.findById({ _id: id }, (err, docs) => {
+    if (err || !docs)
+      return res.status(400).json({
+        error: "User not found",
+      });
+    req.profile = docs;
+    next();
+  });
+};
