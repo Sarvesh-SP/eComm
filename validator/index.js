990832f331d69b015b9bc5eeb961007776ca531a
Sarvesh SP <sarvesh.sp2013@gmail.com>
2021-08-26 11:36:52 +0530

    added signup validator

--- validator/index.js
+++ validator/index.js
@@ -1,11 +1,11 @@
 const { body, validationResult } = require("express-validator");
+const User = require("../models/user");
 
 exports.userSignupValidator = (req, res, next) => {
   const errors = validationResult(req);
-  if (errors) {
-    const first = errors.array().map((error) => error.msg);
-
-    return res.status(400).json({ error: first });
+  console.log(errors);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
   next();
 };
@@ -29,4 +29,14 @@ exports.customErrors = [
     .withMessage("Password must contain at least 6 characters")
     .matches(/\d/)
     .withMessage("Password must contain a number."),
+  body("email").custom((value) => {
+    return User.findOne({ email: value }).then((user) => {
+      if (user) return Promise.reject("E-mail already in use.");
+    });
+  }),
+  body("name").custom((value) => {
+    return User.findOne({ name: value }).then((user) => {
+      if (user) return Promise.reject("Name already exists");
+    });
+  }),
 ];
