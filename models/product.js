d6af4d46f470a556cf0ab482cab13d2bc6912f0b
Sarvesh SP <sarvesh.sp2013@gmail.com>
2021-08-28 20:40:40 +0530

    added product schema

---
+++ models/product.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 15,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxlength: 2000,
+    },
+    price: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    image: {
+      data: Buffer,
+      contentType: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
