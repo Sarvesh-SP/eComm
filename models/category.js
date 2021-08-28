d6d6af5d382184d3da4ae5de5a1561fcabffd0ef
Sarvesh SP <sarvesh.sp2013@gmail.com>
2021-08-28 20:39:16 +0530

    added category schema

---
+++ models/category.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const catergorySchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 35,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Category", catergorySchema);
